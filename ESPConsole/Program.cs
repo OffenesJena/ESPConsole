2a28f95c18da4dd3e38a4ba40ca95c999928e4ba
Achim Friedland <github@ahzf.de>
2015-09-18 00:41:15 +0200

    first commit

---
+++ ESPConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESPConsole
+{
+    public class Program
+    {
+
+        public static SerialPort COMPort;
+
+        public static void Main(String[] Arguments)
+        {
+
+            foreach (var _COMPort in SerialPort.GetPortNames())
+                Console.WriteLine(_COMPort);
+
+            COMPort = new SerialPort("COM14", 9600, Parity.None, 8, StopBits.One);
+            COMPort.DataReceived += (s, e) => Console.Write(COMPort.ReadExisting());
+            COMPort.ReadTimeout  = 4000;
+            COMPort.WriteTimeout = 6000;
+
+            try
+            {
+                COMPort.Open();
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            do
+            {
+                COMPort.WriteLine(Console.ReadLine());
+                Thread.Sleep(250);
+            } while (true);
+
+        }
+
+    }
+
+}
